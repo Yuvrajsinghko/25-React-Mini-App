8eea14e5438dbef43934eb94909c5cce1c12c005
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-02-24 23:48:31 +0530

    added new component to App.jsx

--- Hex-RGB-Color-Generator/src/App.jsx
+++ Hex-RGB-Color-Generator/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import RandomColorGenerator from './component/random-color'
 import StarRating from './component/star-rating'
+import ImageSlider from './component/imageSlider'
+import Timer from './component/practice'
 
 const App = () => {
   return (
     <div>
     {/* <RandomColorGenerator /> */}
-    <StarRating />
+    {/* <StarRating /> */}
+    {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={'4'} page={'3'} /> */}
+    <Timer />
     </div>
   )
 }
