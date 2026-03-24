0d21b7aaef9298effa52efbd2177501a06d85e74
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-24 18:04:51 +0530

    added new small project Nested-sidebar

--- Hex-RGB-Color-Generator/src/App.jsx
+++ Hex-RGB-Color-Generator/src/App.jsx
@@ -6,16 +6,19 @@ import Timer from "./component/practice";
 import Prac from "./component/practice";
 import Counter from "./component/practice";
 import InfiniteDataScroll from "./component/load-more-data";
+import TreeView from "./component/Nested-sidebar";
+import menus from "./component/Nested-sidebar/data";
 
 const App = () => {
   return (
-    <div>
+    <div className="text-amber-200">
       {/* <RandomColorGenerator /> */}
       {/* <StarRating /> */}
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={'5'} page={'3'} /> */}
       {/* <Timer /> */}
       {/* <Counter /> */}
-      <InfiniteDataScroll url={'https://dummyjson.com/products?limit=20'} />
+      {/* <InfiniteDataScroll url={'https://dummyjson.com/products?limit=20'} /> */}
+      <TreeView menus={menus} />
     </div>
   );
 };
---
+++ Hex-RGB-Color-Generator/src/component/Nested-sidebar/menu-list.jsx
@@ -0,0 +1,13 @@
+import MenuItems from "./menu-item";
+import "./style.css"
+const MenuList = ({ list = [] }) => {
+  return (
+    <ul className="menu-list-container">
+      {list && list.length
+        ? list.map((listItem,idx) => <MenuItems key={idx} item={listItem} />)
+        : null}
+    </ul>
+  );
+};
+
+export default MenuList;
