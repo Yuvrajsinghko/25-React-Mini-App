fd3317cb530fc3655b14c8984c46d10e50d7895c
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-05-01 17:45:25 +0530

    updated some files

--- React-Accordian/src/App.jsx
+++ React-Accordian/src/App.jsx
@@ -1,11 +1,13 @@
 import Accordian2 from "./component/accordian2/acc";
+import ModalTest from "./component/Modal-popup/modal-test";
 import QCode from "./component/QR-Code_generator";
 import RandomColorGenerator from "./component/RandomColor_Generator";
+import SearchBox from "./component/SearchBox";
 import StarRating from "./component/StarRating";
 import LightDarkMode from "./component/Theme-CHange";
 
-
 const App = () => {
+
   return (
     <div>
       {/* <Accordian /> */}
@@ -14,7 +16,10 @@ const App = () => {
       {/* <RandomColorGenerator /> */}
       {/* <StarRating noOfStars={10}/> */}
       {/* <QCode /> */}
-      <LightDarkMode />
+      {/* <LightDarkMode /> */}
+      {/* <ModalTest /> */}
+      <SearchBox />
+   
     </div>
   );
 };
