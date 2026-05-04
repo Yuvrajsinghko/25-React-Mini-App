dea930688acd802b154642c0f692cc5e79afbe3e
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-05-04 23:22:28 +0530

    updated some files

--- React-Accordian/src/App.jsx
+++ React-Accordian/src/App.jsx
@@ -2,6 +2,7 @@ import Accordian2 from "./component/accordian2/acc";
 import ModalTest from "./component/Modal-popup/modal-test";
 import QCode from "./component/QR-Code_generator";
 import RandomColorGenerator from "./component/RandomColor_Generator";
+import ScrollIndicator from "./component/Scroll Indicator";
 import SearchBox from "./component/SearchBox";
 import StarRating from "./component/StarRating";
 import LightDarkMode from "./component/Theme-CHange";
@@ -18,7 +19,8 @@ const App = () => {
       {/* <QCode /> */}
       {/* <LightDarkMode /> */}
       {/* <ModalTest /> */}
-      <SearchBox />
+      {/* <SearchBox /> */}
+      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
    
     </div>
   );
