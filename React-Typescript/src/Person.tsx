f9f3c2541d3f5236fbdeeec184c83fbcd825c522
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-05-27 16:50:29 +0530

    practices with form and change event using typescript

--- React-Typescript/src/Person.tsx
+++ React-Typescript/src/Person.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export interface Person {
   name: string;
@@ -7,22 +7,31 @@ export interface Person {
 }
 
 const Person = (props: Person) => {
-  const [isShowInfo, setShowInfo] = useState<boolean>(false);
+  const [bio, setBio] = useState<string | null>(null);
 
-  const toggleInfo=()=>{
-    setShowInfo((prev)=>!prev);
-  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBio(e.target.value);
+  };
 
+  // For OnSubmit
+  const handleSubmit = (e: React.FocusEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      {isShowInfo && (
-        <>
-          <h2>Name:{props.name}</h2>
-          <p>Age{props.age}</p>
-          <p>{props.isTall ? "Person Is tall" : "Person Not Tall"}</p>
-        </>
-      )}
-      <button onClick={toggleInfo} style={{padding:"5px 5px", borderRadius:"20px", border:"none"}}>Toggle Info</button>
+      <h2>Name:{props.name}</h2>
+      <p>Age{props.age}</p>
+      <p>{props.isTall ? "Person Is tall" : "Person Not Tall"}</p>
+
+      <button
+        style={{ padding: "5px 5px", borderRadius: "20px", border: "none" }}
+      >
+        Toggle Info
+      </button>
+      <p>
+        {props.name} Bio:{!bio ? "No Bio Available" : bio}
+      </p>
+      <input onChange={handleChange} type="text" />
     </div>
   );
 };
